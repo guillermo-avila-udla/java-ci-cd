0eee00c17c51f981ff1d1a24d098673a159018cc
guillermo-avila-udla <guillermo.avila@udla.edu.ec>
2025-06-22 20:17:48 +0000

    Agregar checkstyle

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         try (PrintWriter w = new PrintWriter("output/index.html", "UTF-8")) {
             w.println("<h1>Hello from Java CI/CD!</h1>");
+            w.println("<p><a href="checkstyle.html">Ver reporte Checkstyle</a></p>");
             System.out.println("HTML generado correctamente.");
         } catch (IOException e) {
             e.printStackTrace();
